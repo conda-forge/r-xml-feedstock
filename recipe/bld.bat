c41a257ee9f233a7929d33274a3434cee94998ca
Marcel Bargull <marcel.bargull@udo.edu>
2022-10-18 13:29:13 +0200

    Try xml-config --prefix=%LIBRARY_PREFIX%/mingw-w64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,2 @@
-del %LIBRARY_LIB%\xml2.lib
-"%R%" CMD INSTALL --build .
+"%R%" CMD INSTALL --configure-args="--with-xml-config='%LIBRARY_PREFIX%/mingw-w64/bin/xml2-config --prefix=%LIBRARY_PREFIX%/mingw-w64'" --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
