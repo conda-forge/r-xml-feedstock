4ab871ab65a323d7f8708c5acdcf3dee6a570633
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-07-25 16:22:52 +0200

    no configure in cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cp $RECIPE_DIR/Makevars src/Makevars
-
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+if [ ${CONDA_BUILD_CROSS_COMPILATION} -eq 1 ]; then
+    cp $RECIPE_DIR/Makevars src/Makevars
+    ${R} CMD INSTALL --build --no-configure . ${R_ARGS}
+else
+    ${R} CMD INSTALL --build . ${R_ARGS}
+fi
 
 ## BEGIN DEBUG
 cat src/Makevars
