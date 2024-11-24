3f715ea037da76c9caaa9b04d4961a974ca1f9f3
Mervin Fansler <mmfansler@gmail.com>
2024-11-24 18:19:20 +0100

    copy additional file; manually fill in configure vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,11 @@ export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
 if [ ${CONDA_BUILD_CROSS_COMPILATION} -eq 1 ]; then
-    cp $RECIPE_DIR/Makevars src/Makevars
+    cp $RECIPE_DIR/post_configure_files/Makevars src/Makevars
+    cp $RECIPE_DIR/post_configure_files/supports.R R/supports.R
+    sed "s|@LD_PATH@|${PREFIX}/lib|g" inst/scripts/RSXML.bsh.in > inst/scripts/RSXML.bsh
+    sed "s|@LD_PATH@|${PREFIX}/lib|g" inst/scripts/RSXML.csh.in > inst/scripts/RSXML.csh
     ${R} CMD INSTALL --build --no-configure . ${R_ARGS}
 else
     ${R} CMD INSTALL --build . ${R_ARGS}
 fi
-
-## BEGIN DEBUG
-cat src/Makevars
-cat R/supports.R
-## END DEBUG
